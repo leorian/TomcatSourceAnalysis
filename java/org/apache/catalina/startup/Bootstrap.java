e8eb002778f163c4197f44d1e8d106bc793c5f6a
xiezg <xiezg@317hu.com>
2017-02-23 21:09:31 +0800

    日志

--- java/org/apache/catalina/startup/Bootstrap.java
+++ java/org/apache/catalina/startup/Bootstrap.java
@@ -22,6 +22,7 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
+import java.util.logging.Logger;
 
 import org.apache.catalina.Globals;
 import org.apache.catalina.security.SecurityClassLoad;
